c85b3ea23dff74c680804469627715f659f163bb
djgu <djgu@tmindtech.com>
2019-11-08 15:50:24 +0800

    add props

--- src/components/SvgIcon/index.tsx
+++ src/components/SvgIcon/index.tsx
@@ -3,12 +3,11 @@ import PropTypes from "prop-types";
 import "./index.scss";
 
 const SvgIcon = props => {
-  const { name, color, size } = props;
-
+  const { name, color, size, svgProps, useProps } = props;
   return (
     <i aria-hidden="true" className="anticon">
-      <svg className="svg-class" style={{ width: `${size}px`, height: `${size}px` }}>
-        <use xlinkHref={"#icon-" + name} fill={color} />
+      <svg className="svg-class" style={{ width: `${size}px`, height: `${size}px` }} {...svgProps} >
+        <use xlinkHref={"#icon-" + name} fill={color} {...useProps}/>
       </svg>
     </i>
   );
@@ -20,6 +19,8 @@ SvgIcon.propTypes = {
   // 填充颜色
   color: PropTypes.string,
   size: PropTypes.number,
+  svgProps: PropTypes.any,
+  useProps: PropTypes.any,
 };
 
 SvgIcon.defaultProps = {
