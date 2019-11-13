b5a9911f6db58ed8ffed68999c6152524bbd187f
djgu <djgu@tmindtech.com>
2019-11-13 11:38:35 +0800

    add login

--- src/framework/login/index.tsx
+++ src/framework/login/index.tsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
-import { Input, Button } from 'antd'
+import { TForm, Button } from '@tmind/yuna'
 import { userService } from '@/service';
 import { updateUser } from '@/action/setting';
 import store from '@/reduxes';
 import { PathConfig } from '../routes';
 
 import './index.scss';
+import { IFormItem } from '@tmind/yuna/lib/Form';
 
 interface Props{
   history?: any;
@@ -13,25 +14,68 @@ interface Props{
 
 export class Login extends Component<Props> {
 
-  userRefs: any;
-  pwdRefs: any;
+  loginform: any;
 
-  onClick = async () => {
-    const username = this.userRefs.state.value;
-    const password = this.pwdRefs.state.value;
-    console.log(username, password)
-    const [err, data] = await userService.login({username, password})
-    console.log(data, this.props)
-    store.dispatch(updateUser({...data, isLogin: true}))
-    this.props.history.push(PathConfig.home)
-  }
+  onInputKeyPress: (e: React.KeyboardEvent<HTMLInputElement>) => void = (e) => {
+		if (e.which === 13) {
+			this.handleSubmit();
+			e.stopPropagation();
+		}
+	}
+	handleSubmit = () => {
+		this.loginform.validateFields( async (err: any, value: ILoginParams) => {
+			if (!err) {
+				console.log(value)
+				const [err, data] = await userService.login(value)
+        if(!err) {
+          store.dispatch(updateUser({...data, isLogin: true}))
+          this.props.history.push(PathConfig.home)
+        }
+			}
+		});
+	}
+
   render() {
+    const formItems: IFormItem[] = [
+			{
+				id: 'username',
+				initialValue: 'admin',
+				label: '账户',
+				placeholder: '请输入账户名',
+				type: 'input',
+				required: '请输入账户名',
+				props: {
+					onKeyDown: this.onInputKeyPress
+				}
+			},
+			{
+				id: 'password',
+				label: '密码',
+				placeholder: '请输入密码',
+				type: 'password',
+				required: '请输入密码',
+				props: {
+					onKeyDown: this.onInputKeyPress
+				}
+			}
+		];
+
     return (
-      <div>
-        Login111
-        <Input width={200} ref={c => this.userRefs = c} />
-        <Input width={200} ref={c => this.pwdRefs = c} />
-        <Button onClick={this.onClick}>确认</Button>
+      <div className="g-login">
+        <div className="login-content">
+          <div className="login-title"><h2>登录</h2></div>
+          <div className="login-form">
+            <TForm
+              hideRequiredMark
+              formItems={formItems}
+              ref={c => this.loginform = c}
+              itemLayout={{ labelCol: { span: 4 }, wrapperCol: { span: 20 } }}
+            />
+          </div>
+          <div className="login-btn">
+            <Button type="primary" onClick={this.handleSubmit}>登录</Button>
+          </div>
+        </div>
       </div>
     )
   }
