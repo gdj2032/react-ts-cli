ff69c35ffef0a813ad97065641dd1050f5830b04
djgu <djgu@tmindtech.com>
2019-11-19 11:31:53 +0800

    添加代理proxy

--- src/constants/api.tsx
+++ src/constants/api.tsx
@@ -2,7 +2,7 @@ export const mockSwitch = true; // mock开关
 const isProduction = process.env.NODE_ENV !== 'development';
 const Credentials = 'include'; // include 跨域使用 、 same-origin 同源使用
 
-let API_HOST = 'http://192.168.1.179:3721'
+let API_HOST = 'http://192.168.1.179:9001'
 
 if (isProduction) {
   API_HOST = ''; //
