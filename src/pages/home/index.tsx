edd51b841aa04af6965f47db6ffe2adcc7e6cfd4
djgu <djgu@tmindtech.com>
2019-12-12 11:17:42 +0800

    add react-redux @connect 装饰器 babel-plugin-transform-decorators-legacy

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,22 +3,26 @@ import * as React from 'react';
 import './index.scss';
 import { PathConfig } from '@/framework/routes';
 import SvgIcon from '@/components/SvgIcon';
+import { connect } from '@/utils';
 
 interface Props {
   history?: any;
+  user?: any;
 }
 
+@connect
 class Home extends React.Component<Props> {
   onGoto = () => {
-    console.log(this.props)
     this.props.history.push(PathConfig.homeDetail)
   }
   render() {
+    console.log(this.props)
     return (
       <div className="g-home">
         <div className="home">Home</div>
         <SvgIcon name="succeed" color="red" size={50} />
         <div onClick={this.onGoto}>goto</div>
+        <div>connect: {this.props.user.username}</div>
       </div>
     )
   }
